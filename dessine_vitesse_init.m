33b1151661e3cd862455a229bb9d608f94ca33a6
MacILL <84464687+MacILL@users.noreply.github.com>
2021-05-19 17:58:09 +0200

    Add files via upload
    Dessin du vecteur vitesse initiale

---
+++ dessine_vitesse_init.m
@@ -0,0 +1,12 @@
+function dessine_vitesse_init(v0x, v0y, x_init, y_init )
+%dessine_vitesse_init(v0x, v0y )
+%dessine le vecteur vitesse initiale
+
+%date 30 avril 2021
+%Autrice CD
+%Version 1.1
+
+quiver(x_init, y_init, v0x , v0y)
+
+end
+
